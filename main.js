8c39a014d957fe43a58195d4d167487631e7b213
Daniel001-odii <xenithheight@gmail.com>
2023-09-06 01:53:18 +0100

    updates!

--- main.js
+++ main.js
@@ -14,6 +14,15 @@ function checkVisits(){
 console.log("number of visits: ", localStorage.getItem("user_downloads"));
 
 
+
+
+
+
+
+
+
+
+
   
   let getVidBtn = document.getElementById("getVidBtn");
     const yt_vid_details = document.getElementById("yt_vid_details");
@@ -35,8 +44,8 @@ console.log("number of visits: ", localStorage.getItem("user_downloads"));
     setInterval(function(){error_screen.innerHTML = ''}, 8000);
     }
 
-    // const api_url = "http://127.0.0.1:5000"
-    const api_url = "https://techzone-form-ccb2240b74a4.herokuapp.com"
+    const api_url = "http://127.0.0.1:5000"
+    // const api_url = "https://techzone-form-ccb2240b74a4.herokuapp.com"
 
      function fetchVideoInfo() {
         checkVisits();
@@ -57,6 +66,7 @@ console.log("number of visits: ", localStorage.getItem("user_downloads"));
                 fetch(`${api_url}/api/video_info?url=${encodeURIComponent(url)}`)
                     .then(response => response.json())
                     .then(data => {
+                        
                         const resolutionsList = document.getElementById('resolutionsList');
                         const sizeList = document.getElementById('sizeList');
                         const tableBody = document.getElementById('tableBody');
@@ -66,7 +76,13 @@ console.log("number of visits: ", localStorage.getItem("user_downloads"));
                             const row = document.createElement('tr');
 
                             const resolutionCell = document.createElement('td');
-                            resolutionCell.innerHTML = resolution.format + "-" + `<span class="badge bg-success">${resolution.resolution}</span>` ;
+
+                            function checkAudio(){
+                            const hasAudio = resolution.hasAudio;
+                            if(hasAudio == false){return " <i style='color:red' class='bi bi-volume-mute-fill'></i>"}else{return " "};
+                            };
+
+                            resolutionCell.innerHTML = resolution.format + "-" + `<span class="badge bg-success">${resolution.resolution}</span>` + checkAudio();
                             row.appendChild(resolutionCell);
 
                             const sizeCell = document.createElement('td');
@@ -100,11 +116,11 @@ console.log("number of visits: ", localStorage.getItem("user_downloads"));
                         // Populate audio formats
                 // Display best audio download if available
                 if (data.audio) {
-                    console.log("audio file found!")
+                    // console.log("audio file found!")
                     const audioRow = document.createElement('tr');
 
                     const audioFormatCell = document.createElement('td');
-                    audioFormatCell.innerHTML = "<span class='badge bg-info text-dark'>Audio</span> - " + data.audio.format;
+                    audioFormatCell.innerHTML = "<span  class='badge bg-info'>Audio <i class='bi bi-soundwave'></i></span> - " + data.audio.format;
                     audioRow.appendChild(audioFormatCell);
 
                     const audioSizeCell = document.createElement('td');
@@ -130,6 +146,49 @@ console.log("number of visits: ", localStorage.getItem("user_downloads"));
                         getVidBtn.innerHTML = "Get New video";
                         getVidBtn.disabled = false;
                         yt_vid_details.style.display = "block";
+
+
+
+                       // Get the current date and time
+const currentDate = new Date();
+
+// Format it in standard form
+const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+const formattedDate = currentDate.toLocaleString('en-US', options);
+
+// Display the formatted date and time
+// document.getElementById('datetime').textContent = formattedDate;
+
+const input_box = document.getElementById("urlInput");
+let buttons = document.getElementsByClassName("download_btn");
+
+// Load the existing user record from local storage or create an empty array if it doesn't exist
+let temp_store = JSON.parse(localStorage.getItem("user_record")) || [];
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        const record = {
+            "youtube_url": document.getElementById('urlInput').value,
+            "download_url": buttons[i].href,
+            "date_and_time": formattedDate,
+            "thumbnail": data.thumbnail_url,
+        };
+
+        // Push the record to the temp_store array
+        temp_store.push(record);
+
+        // Store the temp_store array as a JSON string in local storage
+        localStorage.setItem("user_record", JSON.stringify(temp_store));
+    };
+}
+
+
+
+
+                        
+
+
+
                         
                     })
                     .catch(error => {
