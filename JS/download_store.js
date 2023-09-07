585e4358d551d0d6345007d91987cc1843180f28
Daniel001-odii <xenithheight@gmail.com>
2023-09-07 16:54:22 +0100

    removed Js folder

--- JS/download_store.js
+++
@@ -1,8 +0,0 @@
-// const input_box = document.getElementById("urlInput");
-//                         let buttons = document.getElementsByClassName("download_btn");
-//                         for(let i = 0; i <= buttons.length; i++){
-//                             buttons[i].onclick = function(){
-//                             alert("you cicked a button");
-//                             console.log("you cicked a button");
-//                             }
-//                         }
