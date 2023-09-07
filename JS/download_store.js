961e7c4cfb954c4b0a4f0c733df13ebef2a7b29e
Daniel001-odii <xenithheight@gmail.com>
2023-09-07 17:12:13 +0100

    all working version

---
+++ JS/download_store.js
@@ -0,0 +1,8 @@
+// const input_box = document.getElementById("urlInput");
+//                         let buttons = document.getElementsByClassName("download_btn");
+//                         for(let i = 0; i <= buttons.length; i++){
+//                             buttons[i].onclick = function(){
+//                             alert("you cicked a button");
+//                             console.log("you cicked a button");
+//                             }
+//                         }
